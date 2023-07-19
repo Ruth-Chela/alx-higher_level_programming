a68871ba3ac81983cae1a2fb76ef57b2eaeeefc8
Ruth-Chela <karutto18@gmail.com>
2023-07-19 10:53:18 -0700

    smores

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module for test Rectangle class """
+import unittest
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class TestRectangleMethods(unittest.TestCase):
+    """ Suite to test Rectangle class """
+
+    def setUp(self):
+        """ Method invoked for each test """
+        Base._Base__nb_objects = 0
+
+    def test_new_rectangle(self):
+        """ Test new rectangle """
