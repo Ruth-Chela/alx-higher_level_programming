3a2fac6c8e5e22c54eb873ce0a4896b4364943c8
Ruth-Chela <karutto18@gmail.com>
2023-07-19 10:45:36 -0700

    case files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module that contains class Base """
+import json
+import csv
+import os.path
+
+
+class Base:
+    """ Class Base """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initializes instances """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ List to JSON string """
+        if list_dictionaries is No
